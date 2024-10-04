b44e5a4cc57e159f3a096865bdf7ec4600bd4306
Hayyuu <hayatahimed518@gmail.com>
2024-10-04 18:37:58 +0300

    Modify insertAt method

--- linkedListFactory.mjs
+++ linkedListFactory.mjs
@@ -19,24 +19,47 @@ class LinkedList{
     }
     getNodeAt(index){
         let node=this.head;
+
         for(let i=0;i<this.length;i++){
+
             if(i==index){
                 return node;
             }
-            node=node.next;
+            else if(index > this.length-1){
+                return;
+
+            }
+            else{
+               if(node!=this.getLastNode()){
+                 node=node.next;
+               }
+            }
+            
+
         }
+        return node;
+       
     }
-    append(newNode){
-        let lastNode=this.getLastNode();
-        lastNode.next=newNode;
-        console.log(lastNode);
+    append(value){
+        let node=new Node(value);
+        if(this.length===0){
+            this.head=node;
+            node.next=null;
+        }
+        else{
+            let lastNode=this.getLastNode();
+            node.next=null;
+            lastNode.next=node;
+            console.log(lastNode);
+           
+        }
         this.length++;
         return this;
     }
-    prepend(newNode){
-        let nextNode=this.head.next;
-        this.head=newNode;
-        this.head.next=nextNode;
+    prepend(value){
+        let node=new Node(value);
+        node.next=this.head;
+        this.head=node;
         this.length++;
         return this;
     }
@@ -77,57 +100,75 @@ class LinkedList{
         let str="";
         let node=this.head;
         for(let i=0;i<this.length;i++){
+            console.log("Inside tostring"+ node.value)
+            if(node==this.getLastNode()){
+                console.log(node);
+                str+=node.value;
+                str+=" -> "+node.next
+                return str;
+              }
             str+=node.value +" -> ";
             node=node.next;
-            if(node==null){
-              str+=node;
-            }
+            
         }
         
         return str;
     }
     insertAt(value,index){
-        let currentNode=this.head;
-        for(let i=0;i<this.length;i++){
-            // currentNode=this.getNodeAt(i);
-            if(i==index){
-                let newNode=new Node(value);
-                newNode.next=currentNode.value;
-                currentNode.value=newNode.value;
-                this.length++;
-                return this;
-            }
-            currentNode=currentNode.next;
+        if(index==0){
+            this.prepend(value);
         }
+        else{
+            let node=new Node(value);
+            let prevNode,currentNode;
+            currentNode=this.head;
+            prevNode=null;
+            for(let i=0;i<index;i++){
+               prevNode=currentNode;
+               currentNode=currentNode.next;
+            }
+            node.next=currentNode;
+            prevNode.next=node;
+            this.length++;
+        }
+        
         return this;
     }
-    
+  
 }
 
 let linkedListObj=new LinkedList(new Node());
-let node1=new Node(1);
-let node2=new Node(2);
-console.log(linkedListObj.append(node1));
-console.log(linkedListObj.append(node2));
 
-let firstNode=new Node("First Node");
-let lastNode=new Node("End Node");
+console.log(linkedListObj);
+console.log("Before First Append");
+console.log(linkedListObj.append(1));
+console.log(linkedListObj.append(2));
 
-console.log(linkedListObj.prepend(firstNode));
+
+console.log("Before prepend");
+console.log(linkedListObj.prepend("First Node"));
 console.log(linkedListObj.size());
-console.log(linkedListObj.append(lastNode));
+console.log(linkedListObj.append("End Node"));
 console.log(linkedListObj.size());
 
 console.log(`last node ${linkedListObj.getLastNode().value}`);
 console.log(`Head ${linkedListObj.getHead()}`)
 
-console.log(`Node at index 1 has a value of ${linkedListObj.getNodeAt(3).value}`)
-
+console.log(`Node at index 3 has a value of ${linkedListObj.getNodeAt(3).value}`)
 console.log(linkedListObj.pop());
+console.log("After Pop"+linkedListObj.size());
+
 console.log(`List contains the value 6 ${linkedListObj.contains(6)}`);
 console.log(`Index of 'First Node' is ${linkedListObj.findIndex('First Node')}`);
 console.log(`String representation ${linkedListObj.toString()}`);
-console.log(linkedListObj.insertAt(7,1));
-console.log(linkedListObj.getNodeAt(1));
-// console.log(linkedListObj);
+console.log('Before Insert');
+console.log(linkedListObj.size());
 
+console.log(linkedListObj.insertAt(7,0));
+console.log(linkedListObj.size());
+
+console.log(linkedListObj.insertAt(8,1));
+
+console.log(linkedListObj.toString());
+console.log(linkedListObj.size());
+console.log ('end')
